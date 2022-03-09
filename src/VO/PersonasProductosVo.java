bda0e704c1e64f35fc1ea5aea46e090691ce17f6
estiben velasco <stivencv7@gmail.com>
2022-03-09 11:49:42 -0500

    Concluí la clase PersonasProductoVo y Finalice el modelo VO

--- src/VO/PersonasProductosVo.java
+++ src/VO/PersonasProductosVo.java
@@ -1,5 +1,41 @@
 package VO;
 
 public class PersonasProductosVo {
+	private Long personaId;
+	private Long productoId;
+	
+	public PersonasProductosVo() {
+		
+	}
 
+	public PersonasProductosVo(Long personaId, Long productoId) {
+		super();
+		this.personaId = personaId;
+		this.productoId = productoId;
+	}
+
+	public Long getPersonaId() {
+		return personaId;
+	}
+
+	public void setPersonaId(Long personaId) {
+		this.personaId = personaId;
+	}
+
+	public Long getProductoId() {
+		return productoId;
+	}
+
+	public void setProductoId(Long productoId) {
+		this.productoId = productoId;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonasProductosVo [personaId=" + personaId + ", productoId=" + productoId + "]";
+	}
+	
+	
+
+	
 }
