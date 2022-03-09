4c454076476399520686cc1df7fef854757bf36e
estiben velasco <stivencv7@gmail.com>
2022-03-09 10:56:56 -0500

    construí  el modelo VO y sus respectivas clases perosonaVO ya realizada

---
+++ src/VO/PersonaVo.java
@@ -0,0 +1,116 @@
+package VO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonaVo {
+	
+	private long idPesona;
+	private String nombre;
+	private String telefono;
+	private String profesion;
+	private int tipo;
+
+	private Nacimiento nacimiento;
+	private List<MascotaVo> listaMascotas;
+
+
+	public PersonaVo() {
+		this.listaMascotas=new ArrayList<MascotaVo>();
+	}
+
+
+	public PersonaVo(long idPesona, String nombre, String telefono, String profesion, int tipo, Nacimiento nacimiento) {
+		super();
+		this.idPesona = idPesona;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.profesion = profesion;
+		this.tipo = tipo;
+		this.nacimiento = nacimiento;
+		this.listaMascotas = new ArrayList<MascotaVo>();
+	}
+
+
+	public long getIdPesona() {
+		return idPesona;
+	}
+
+
+	public void setIdPesona(long idPesona) {
+		this.idPesona = idPesona;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getProfesion() {
+		return profesion;
+	}
+
+
+	public void setProfesion(String profesion) {
+		this.profesion = profesion;
+	}
+
+
+	public int getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public Nacimiento getNacimiento() {
+		return nacimiento;
+	}
+
+
+	public void setNacimiento(Nacimiento nacimiento) {
+		this.nacimiento = nacimiento;
+	}
+
+
+	public List<MascotaVo> getListaMascotas() {
+		return listaMascotas;
+	}
+
+
+	public void setListaMascotas(List<MascotaVo> listaMascotas) {
+		this.listaMascotas = listaMascotas;
+	}
+
+
+	@Override
+	public String toString() {
+		System.out.println("ingresa");
+		return "PersonaVo [idPesona=" + idPesona +"," 
+				+"nombre=" + nombre + ", telefono=" + telefono + 
+				", profesion="+ profesion + ", tipo=" + tipo +","
+							+" nacimiento=" + nacimiento + "]";
+	}
+	
+	
+
+}
